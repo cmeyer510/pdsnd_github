8903ed9533327277647cf850981ba3ee0fb80b61
Caitlyn Meyer <cmeyer5@mtb.com>
2025-07-25 20:57:06 +0000

    Changed document string for user_stats function

--- bikeshare.py
+++ bikeshare.py
@@ -167,7 +167,7 @@ def trip_duration_stats(df):
     print('-'*40)
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """Displays user type and gender statistics on bikeshare users."""
     
     print('\nCalculating User Stats...\n')
     start_time = time.time()
